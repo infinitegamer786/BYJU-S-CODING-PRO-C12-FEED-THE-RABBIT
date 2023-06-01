e0d62bc1fa5318ad05be5dcf3c3e563ce9a9a9f6
infinitegamer786 <128330032+infinitegamer786@users.noreply.github.com>
2023-06-01 11:23:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,47 +46,47 @@ function draw() {
  var select_sprites = Math.round(random(1,3));
 
   
-  if (frameCount % 50 == 0) {
- if (select_sprites == 1) {
-  createApples();
-  } else if (select_sprites == 2
-     createOrange();
-    }else {
-     createRed();
-  /  }
-   }
+  //if (frameCount % 50 == 0) {
+ //if (select_sprites == 1) {
+  //createApples();
+  //} else if (select_sprites == 2
+     //createOrange();
+    //}else {
+     //createRed();
+  //  }
+   //}
 
-   if (frameCount % 80 == 0) {
-    if (select_sprites == 1) {
-       createApples();
+   //if (frameCount % 80 == 0) {
+   //if (select_sprites == 1) {
+    //createApples();
     } else if (select_sprites == 2) {
-      createOrange();
-     }
-   }
+      //createOrange();
+     //}
+   //}
 
-   if (frameCount / 80 == 0) {
-     if (select_sprites == 1) {
+   //if (frameCount / 80 == 0) {
+     //if (select_sprites == 1) {
       createApples();
-    } else if (select_sprites == 2) {
+    //} else if (select_sprites == 2) {
        createOrange();
-     }else {
-      createRed();
-     }
-  }
+     //}else {
+      //createRed();
+     //}
+  //}
 
-   if (frameCount % 80 = 0) {
-     if (select_sprites == 1) {
-      createApples();
-     } else if (select_sprites == 2) {
-       createOrange();
-    }else {
-       createRed();
-    }
-   }
+   //if (frameCount % 80 = 0) {
+     //if (select_sprites == 1) {
+      //createApples();
+     //} else if (select_sprites == 2) {
+       //createOrange();
+    //}else {
+       //createRed();
+    //}
+   //}
 
 
 
-}
+//}
 
 function createApples() {
 apple = createSprite(random(50, 350),40, 10, 10);
